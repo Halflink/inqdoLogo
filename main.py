9982e5eed17a03d1e002589d81d4dd67570c95bc
Jeroen van zwam <84639361+Halflink@users.noreply.github.com>
2023-09-17 16:52:46 +0200

    changed code: added all characters

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import time
 from neopixel import Neopixel
- 
-numpix = 21
+
+numpix = 63
 pixels = Neopixel(numpix, 0, 28, "GRB")
- 
+neoPixelCharacters = [
+    ['I', [0, 8]],
+    ['N', [9, 20]],
+    ['Q', [21, 39]],
+    ['D', [40, 51]],
+    ['O', [52, 62]]
+]
+
 yellow = (255, 100, 0)
 orange = (255, 50, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 red = (255, 0, 0)
-white= (255, 255, 255)
+white = (255, 255, 255)
 colour0 = white
 colour1 = blue
 colour2 = yellow
@@ -19,21 +26,24 @@ useColour = colour3
 
 pixels.brightness(50)
 pixels.fill(colour0)
- 
+
 while True:
     pixels.fill(colour0)
     pixels.show()
     time.sleep(1)
-    
+
     if useColour == colour3:
         useColour = colour1
     elif useColour == colour2:
         useColour = colour3
     elif useColour == colour1:
         useColour = colour2
-    
-    for counter in range(0, numpix):
-        pixels.set_pixel(counter, useColour)
+
+    for neoPixelCharacter in neoPixelCharacters:
+        character = neoPixelCharacter[0]
+        characterPixelRange = neoPixelCharacter[1]
+        pixels.set_pixel_line(characterPixelRange[0], characterPixelRange[1], useColour)
         pixels.show()
         time.sleep(0.5)
+
 
